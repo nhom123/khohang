ede40f360ef46e4ddbde9f91ec46fd1ad57fb34a
beTun123 <alotab91@gmail.com>
2015-11-19 13:12:38 +0700

    xyyz

---
+++ QuanLyKhoHang/QuanLyKhoHang/Models/Entities/PhieuThuChi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyKhoHang.Models.Entities
+{
+    public class PhieuThuChi
+    {
+    }
+}
